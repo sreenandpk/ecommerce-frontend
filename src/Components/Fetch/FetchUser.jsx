95e22bcd4665497c6e38031b599e9157eccc3a6b
Davidd132 <sreenandpk3@gmail.com>
2025-09-05 01:03:56 +0530

    Updated frontend

--- src/Components/Fetch/FetchUser.jsx
+++ src/Components/Fetch/FetchUser.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://fake-backend-2.onrender.com"; // keep one place for easy change
+const BASE_URL = process.env.REACT_APP_API_URL; // keep one place for easy change
 
 
 // For fetching products by search term
