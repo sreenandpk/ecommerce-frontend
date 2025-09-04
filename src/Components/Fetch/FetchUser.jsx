40b35f16743770ce2719bf7a0980b346df81fbba
Davidd132 <sreenandpk3@gmail.com>
2025-09-05 01:08:00 +0530

    Updated frontend

--- src/Components/Fetch/FetchUser.jsx
+++ src/Components/Fetch/FetchUser.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-const BASE_URL = process.env.REACT_APP_API_URL; // keep one place for easy change
+const BASE_URL = import.meta.env.VITE_API_URL;
+ // keep one place for easy change
 
 
 // For fetching products by search term
